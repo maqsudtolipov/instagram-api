ca3e99468ef0a62230174adf3433517eee245b53
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2022-06-06 17:20:50 +0500

    Create getPost function

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,18 +1,40 @@
 const Post = require('../model/postModel');
 
-exports.getAllPosts = (req, res) => {
-  res.status(200).send({
-    status: 'success',
-    results: 500,
-    data: {},
-  });
+exports.getAllPosts = async (req, res) => {
+  try {
+    const posts = await Post.find();
+    console.log(posts);
+    res.status(200).json({
+      status: 'success',
+      results: posts.length,
+      data: {
+        posts,
+      },
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
 
-exports.getPost = (req, res) => {
-  res.status(200).send({
-    status: 'success',
-    data: {},
-  });
+exports.getPost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        post,
+      },
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
 
 exports.createPost = async (req, res) => {
